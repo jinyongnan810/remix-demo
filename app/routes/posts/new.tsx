8f2ed5f101cc448ddad7d92b7527787d8191df46
Kin Yuunan <yuunan.kin@gmail.com>
2021-12-31 08:11:53 +0900

    actions

--- app/routes/posts/new.tsx
+++ app/routes/posts/new.tsx
@@ -1,5 +1,38 @@
+import { Form, Link, redirect } from "remix";
+import type { ActionFunction } from "remix";
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const title = form.get("title");
+  const body = form.get("body");
+  // TODO: submit to database
+  return redirect("/posts");
+};
+
 const New = () => {
-  return <div>New Post</div>;
+  return (
+    <>
+      <div className="page-header">
+        <h1>New Post</h1>
+        <Link to="/posts" className="btn btn-reverse">
+          Back
+        </Link>
+      </div>
+      <Form method="post">
+        <div className="form-control">
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" id="title" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Content</label>
+          <textarea name="body" id="body" />
+        </div>
+        <button type="submit" className="btn btn-block">
+          Add Post
+        </button>
+      </Form>
+    </>
+  );
 };
 
 export default New;
