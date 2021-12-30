847b64996babeb708bb037b385f70e32873267ee
Kin Yuunan <yuunan.kin@gmail.com>
2021-12-30 11:58:20 +0900

    warp content with layout

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,22 @@
-import { LiveReload, Outlet } from "remix";
+import { Link, LiveReload, Outlet } from "remix";
 
-export default function App(){
+export default function App() {
   return (
-    <Document title={'remix demo'}>
-      <Outlet/>
+    <Document title={"remix demo"}>
+      <Layout>
+        <Outlet />
+      </Layout>
     </Document>
-  )
+  );
 }
 
-function Document({children,title}:{children:React.ReactElement ,title:string}){
+function Document({
+  children,
+  title,
+}: {
+  children: React.ReactElement;
+  title: string;
+}) {
   return (
     <html lang="en">
       <head>
@@ -18,8 +26,26 @@ function Document({children,title}:{children:React.ReactElement ,title:string}){
         {/* load the content */}
         {children}
         {/* enable live reload in dev*/}
-        {process.env.NODE_ENV ==='development' && <LiveReload/>}
+        {process.env.NODE_ENV === "development" && <LiveReload />}
       </body>
     </html>
-  )
+  );
+}
+
+function Layout({ children }: { children: React.ReactElement }) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          Remix
+        </Link>
+        <ul className="nav">
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="container">{children}</div>
+    </>
+  );
 }
